12d299bd3f14afa93ab65618a254f6e776312a0a
Axel Po <axelpoin@gmail.com>
2023-01-12 23:48:39 +0100

    feat(anims): add animations on scroll

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,21 +1,67 @@
 import React from "react";
+import { useRef, useEffect } from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
 
 export default function About() {
+  const titleRef = useRef(null);
+  const txtRef1 = useRef(null);
+  const txtRef2 = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(titleRef.current, {
+      y: 0,
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: titleRef.current,
+        // markers: true,
+        start: "top-=130% center",
+      },
+    });
+
+    gsap.to(txtRef1.current, {
+      y: 0,
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: txtRef1.current,
+        // markers: true,
+        start: "top-=130% center",
+      },
+    });
+
+    gsap.to(txtRef2.current, {
+      y: 0,
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: txtRef2.current,
+        // markers: true,
+        start: "top-=130% center",
+      },
+    });
+  }, []);
+
   return (
-    <section id='about' className='bg-white py-28  md:py-40'>
+    <section id='about' className='  bg-white py-28  md:py-40'>
       <div className='container grid grid-cols-1 gap-12 md:grid-cols-3'>
-        <h1 className='text-5xl font-bold uppercase md:text-6xl'>
+        <h1
+          ref={titleRef}
+          className='invisible translate-y-full text-5xl font-bold uppercase opacity-0 md:text-6xl'>
           About <br />
           me
         </h1>
 
-        <p className='text-base md:text-lg'>
+        <p
+          ref={txtRef1}
+          className='invisible translate-y-full  text-base opacity-0 md:text-lg'>
           Bonjour, je suis un développeur web frontend spécialisé sur les
           technologies JavaScript (React JS, Next JS, Typescript, ...), grâce à
           cela, je réalise des applications web performantes.
         </p>
 
-        <p className='text-base md:text-lg'>
+        <p
+          ref={txtRef2}
+          className=' invisible translate-y-full text-base opacity-0 md:text-lg'>
           J'accompagne les particuliers, start-up, associations dans la création
           de leur site internet. 🚀 J'ai commencé à apprendre le développement
           web en autodidacte et je suis devenu un réel passionné qui continue à
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,32 +1,63 @@
 import React from "react";
+import { useRef, useEffect } from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
 
 export default function Services() {
+  const titleRef = useRef(null);
+  const txtRef = useRef(null);
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(titleRef.current, {
+      autoAlpha: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: titleRef.current,
+        // markers: true,
+        start: "top-=300 center",
+      },
+    });
+    gsap.to(txtRef.current, {
+      y: 0,
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: txtRef.current,
+        // markers: true,
+        start: "top-=300 center",
+      },
+    });
+    gsap.to(divRef.current, {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: divRef.current,
+        // markers: true,
+        start: "top center",
+      },
+    });
+  }, []);
+
   return (
     <section id='services' className='flex bg-black py-28 text-white md:py-40'>
       <div className='container grid grid-cols-1 gap-11 md:grid-cols-2'>
         <div>
-          <h2 className='text-5xl font-bold md:text-6xl'>
+          <h2
+            ref={titleRef}
+            className='invisible translate-y-full text-5xl font-bold opacity-0 md:text-6xl'>
             Look at my <span className='bg-gradient'>services</span>
           </h2>
-          <p className='mt-12 max-w-[352px] text-gray'>
+          <p
+            ref={txtRef}
+            className='invisible mt-12 max-w-[352px] translate-y-full text-gray opacity-0'>
             I discover this process working more than 10+ in the design industry
             that why you can trust blindly
           </p>
-
-          {/* <div className='mt-12 flex h-[140px] items-center justify-center gap-x-12 overflow-hidden bg-red-500'>
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-            <img src='icons-skills/css.svg' alt='' />
-          </div> */}
         </div>
 
-        <div className='gradient-2 flex w-full flex-col gap-6  p-4 text-black md:p-6'>
+        <div
+          ref={divRef}
+          className='gradient-2 invisible flex w-full flex-col gap-6 p-4  text-black opacity-0 md:p-6'>
           <ServiceCard />
           <ServiceCard />
           <ServiceCard />
@@ -39,8 +70,25 @@ export default function Services() {
 }
 
 export function ServiceCard() {
+  const serviceCardRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(serviceCardRef.current, {
+      autoAlpha: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: serviceCardRef.current,
+        // markers: true,
+        start: "top-=200% center",
+      },
+    });
+  }, []);
   return (
-    <div className='flex bg-white p-5'>
+    <div
+      ref={serviceCardRef}
+      className='invisible flex translate-y-full bg-white p-5 opacity-0'>
       <div className='flex flex-col'>
         <h3 className='text-lg font-bold'>
           Création d'application web React JS
